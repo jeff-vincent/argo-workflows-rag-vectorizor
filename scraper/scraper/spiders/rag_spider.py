40c150de50117aadb40132128b0eafad959ce516
jeff.vincent <jeff.d.vincent@gmail.com>
2024-11-28 11:27:39 -0800

    reads input from configmap like dir; parses multiple urls

--- scraper/scraper/spiders/rag_spider.py
+++ scraper/scraper/spiders/rag_spider.py
@@ -3,21 +3,29 @@ from bs4 import BeautifulSoup
 import os
 
 def get_start_urls():
-    # urls = os.listdir(os.path.join('/path/to/configMap'))
-    urls = ['https://www.mongodb.com/docs/atlas/tutorial/connect-to-your-cluster/']
+    url_files = os.listdir(os.path.join('/Users/jeffvincent/k8s-rag-vectorizor/input'))
+    urls = []
+    for url_file in url_files:
+        with open(os.path.join('/Users/jeffvincent/k8s-rag-vectorizor/input', url_file), 'r') as file:
+            urls.extend(file.readlines())
 
     return urls
 
-class SimpleSpider(scrapy.Spider):
-    name = "simple_spider"
+class RagSpider(scrapy.Spider):
+    name = "rag_spider"
     start_urls = get_start_urls()
 
     def parse(self, response):
-        # Create a BeautifulSoup object
+        output_filepath = None
+        for k, v in response.__dict__.items():
+            if k == '_url':
+                filename = v.split('/')[-2]
+                print(filename)
+                output_filepath = os.path.join(f'/Users/jeffvincent/k8s-rag-vectorizor/data/{filename}.txt')
+
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Open a file for writing extracted content
-        with open("/Users/jeffvincent/k8s-rag-vectorizor/data/example.txt", "w", encoding="utf-8") as file:
+        with open(output_filepath, "w", encoding="utf-8") as file:
             title = soup.title.string if soup.title else "No title"
             file.write(f"Title: {title}\n\n")
             # Iterate through all tags in the body
